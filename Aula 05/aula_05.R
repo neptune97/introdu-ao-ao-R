a5079fd20a401ed75f26edf631902c899b83b251
Fernando <57836771+neptune97@users.noreply.github.com>
2022-10-21 17:55:44 -0300

    aula 05

---
+++ Aula 05/aula_05.R
@@ -0,0 +1,56 @@
+# CURSO - ANALISE DE DADOS EM R -------------------------------------------
+# AULA 05 -----------------------------------------------------------------
+# PROF: FERNANDO DE SOUZA, Me ---------------------------------------------
+
+
+# LEMBRETES ---------------------------------------------------------------
+
+# 1. Para criar um script crie na folha com o sinal de mais, ou digite 
+# CTLR+SHIFT+N
+
+# 2. Para rodar um comando use CTRL + ENTER ou selecione as linhas e
+# clique em "Run"
+
+# 3. Sempre use # para escrever comentários em seus scripts.
+
+# 4. Tente ao máximo deixar comentários nas suas ações, seu eu do futuro
+# ficará imensamente feliz
+
+# 5. Sempre use um script, programar apenas no Console fará com que seus 
+# dados se percam
+
+
+
+# instalações -------------------------------------------------------------
+
+install.packages("rmarkdown")
+install.packages("knitr")
+install.packages("tinytex")
+install.packages("here")
+install.packages("tidyverse")
+install.packages("broom")
+install.packages("fs")
+install.packages("usethis")
+
+
+
+tinytex::install_tinytex()
+
+
+
+library(rmarkdown)
+library(knitr)
+library(tinytex)
+library(here)
+library(tidyverse)
+library(broom)
+library(fs)
+library(usethis)
+
+
+
+
+
+
+
+
